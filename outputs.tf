d105d723dff5fd9485c35ae0b4123b481dc256af
Viktor_Purins <viktors@cwise.eu>
2024-11-28 14:51:41 +0200

    added custom sa mechanism v3

--- outputs.tf
+++ outputs.tf
@@ -3,12 +3,13 @@ output "argocd_kubernetes_namespace" {
 }
 
 output "argocd_gcp_iam_service_account_email" {
-  value = module.my-app-workload-identity[0].gcp_service_account_email
+  value = local.enable_workload_identity ? module.my-app-workload-identity[0].gcp_service_account_email : var.service_acc_name
 }
 
 output "argocd_gcp_iam_service_account_name" {
-  value = module.my-app-workload-identity[0].gcp_service_account_name
+  value = local.enable_workload_identity ? module.my-app-workload-identity[0].gcp_service_account_name : var.service_acc_name
 }
+
 
 output "service_acc_name" {
   value = local.enable_workload_identity ? module.my-app-workload-identity[0].k8s_service_account_name : var.service_acc_name
